14f706356c63224c60630aed1e5e331a4a924612
Aekkarat Klomjit <56069441+aekkaratklomjit@users.noreply.github.com>
2021-08-10 01:09:00 +0700

    Update CheckLocation.js

--- src/screens/CheckLocation.js
+++ src/screens/CheckLocation.js
@@ -26,6 +26,8 @@ class CheckLocation extends Component {
                alert('Your position is not close to the scope of the company.')}
         }else{alert('Please add location or getLocation')}
         console.log('1')
+        console.log('2')
+        console.log('3')
    }
     render() {
         return (
